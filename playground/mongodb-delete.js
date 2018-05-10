6ecfd172047e598f621129ff35026ecfc861d5fe
Zane Riley <zeriley@gmail.com>
2018-05-10 13:46:15 -0600

    adding delete methods

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,20 @@
+const { MongoClient, ObjectID } = require('mongodb')
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server')
+  }
+  console.log('Connected to MongoDB server')
+  const db = client.db('TodoApp')
+
+  // db.collection('Todos').deleteMany({text: 'Eat lunch'})
+  //   .then(result => console.log(result))
+
+  // db.collection('Todos').deleteOne({text: 'Eat lunch'})
+  //   .then(result => console.log(result))
+
+  db.collection('Todos').findOneAndDelete({completed: false})
+    .then(doc => console.log(JSON.stringify(doc, undefined, 2)))
+
+  client.close()
+})
