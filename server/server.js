88cdfc0eb15d0e609ee95743b7ffed7965353c59
Zane Riley <zeriley@gmail.com>
2018-05-16 13:25:25 -0600

    Adding some test cases

--- server/server.js
+++ server/server.js
@@ -10,6 +10,12 @@ const port = 4000
 
 app.use(bodyParser.json())
 
+app.get('/todos', (req, res) => {
+  Todo.find().then(todos => {
+    res.send({todos})
+  }).catch(e => res.status(500).send(e))
+})
+
 app.post('/todos', (req, res) => {
   let todo = new Todo({
     text: req.body.text
@@ -17,9 +23,11 @@ app.post('/todos', (req, res) => {
 
   todo.save()
     .then(doc => res.send(doc))
-    .catch(e => res.send(e).status(500))
+    .catch(e => res.status(500).send(e))
 })
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`)
 })
+
+module.exports = app
